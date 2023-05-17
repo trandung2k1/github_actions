f9bd1f505dafb1ce301e924f70ce13fb96913c4e
trandung2k1 <trandungitksnb2001@gmail.com>
2023-05-17 21:27:37 +0700

    create bug unit test

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -9,7 +9,7 @@ describe('APP should say "Hello World!"', () => {
     test('should return 200', async () => {
         const res = await request(server).get("/")
         expect(res.status).toBe(200);
-        expect(res.body).toMatchObject({ message: `Hello World!` });
+        expect(res.body).toMatchObject({ message: `Hello World! Create bug` });
 
     });
     afterAll((done) => {
